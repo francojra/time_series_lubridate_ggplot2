af209f658b2e7f31dd304c68dfba74d86cb39404
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-24 23:24:15 -0300

    Add files via upload

---
+++ time_series_ggplot2_lubridate.R
@@ -0,0 +1,17 @@
+
+# Time series - ggplot2 e lubridate --------------------------------------------------------------------------------------------------------
+# Autoria do script ------------------------------------------------------------------------------------------------------------------------
+# Data: 23/05/22 ---------------------------------------------------------------------------------------------------------------------------
+
+# Conceito ---------------------------------------------------------------------------------------------------------------------------------
+
+### Time series estuda a evolução de uma ou mais variáveis ao longo do tempo.
+### Existe um foco sobre o tidyverse: o pacote lubridate é o melhor para lidar
+### com o formato de datas, e o ggplot2 faz o gráfico de forma eficiente. O
+### pacote dygraphs é também considerado para produzir incríveis gráficos
+### interativos. 
+
+# Formato de datas -------------------------------------------------------------------------------------------------------------------------
+
+
+
